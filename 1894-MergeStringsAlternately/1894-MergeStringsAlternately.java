4507b528302117d15b85dfd6c2532cb2242ad6c3
Jeeva8826 <jeevananth1234@gmail.com>
2026-08-11 15:59:06 +0530

    Create 1894-MergeStringsAlternately

---
+++ 1894-MergeStringsAlternately/1894-MergeStringsAlternately.java
@@ -0,0 +1,29 @@
+// Last updated: 8/11/2026, 3:59:05 PM
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+
+        StringBuilder result = new StringBuilder();
+
+        int i = 0;
+        int j = 0;
+
+        while (i < word1.length() && j < word2.length()) {
+            result.append(word1.charAt(i));
+            result.append(word2.charAt(j));
+            i++;
+            j++;
+        }
+
+        while (i < word1.length()) {
+            result.append(word1.charAt(i));
+            i++;
+        }
+
+        while (j < word2.length()) {
+            result.append(word2.charAt(j));
+            j++;
+        }
+
+        return result.toString();
+    }
+}
